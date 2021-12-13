86bc048b89fe3085a27b03fd1e2f7e62b6613b6f
Priyanka Kuklani <priyankakuklani@priyankas-mbp.home>
2021-12-12 20:17:04 -0500

    Updating DB, adding UI changes

--- src/bbank/DB/DButil.java
+++ src/bbank/DB/DButil.java
@@ -1,6 +1,5 @@
 package bbank.DB;
 
-
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -17,33 +16,32 @@ import java.util.logging.Logger;
  */
 public class DButil implements DBConnection {
 
-    private String dburl= "jdbc:sqlserver://localhost:1433;databaseName=bloodbank";
-     private  String user = "SA";
-private String pass = "Mypass@word1";
-   public DButil() {
-       
-    this.dburl=dburl;
-    this.pass=pass;
-    this.user=user;
-       
-  }
-       
-  public Connection getConnection()
-  {
+    //private String dburl= "jdbc:sqlserver://localhost:1433;databaseName=bloodbank";
+    private String dburl = "jdbc:sqlserver://localhost:1433;databaseName=master";
+    private String user = "SA";
+    private String pass = "Mypass@word1";
 
-   
+    public DButil() {
+
+        this.dburl = dburl;
+        this.pass = pass;
+        this.user = user;
+
+    }
+
+    public Connection getConnection() {
+
         try {
             Connection conn = DriverManager.getConnection(dburl, user, pass);
-             System.out.println(" dbutil Connected to database !");
-             return conn;
+            System.out.println(" dbutil Connected to database !");
+            return conn;
         } catch (SQLException ex) {
-             System.out.println(" dbutil catch not Connected to database !");
+            System.out.println(" dbutil catch not Connected to database !");
             Logger.getLogger(DButil.class.getName()).log(Level.SEVERE, null, ex);
-       
-        }
-           
- return null;
-}  
-    
-}
 
+        }
+
+        return null;
+    }
+
+}
